ac2bd1c4b14f4b1b2df73ccd4db31947304cb1c1
staveliu <30430783+staveliu@users.noreply.github.com>
2017-07-26 09:22:27 +0800

    changed

--- questionnaire/config/class.php
+++ questionnaire/config/class.php
@@ -7,11 +7,14 @@ class user{
 		$b=rand(100000,999999);
 		$str=$a.$b;
 	    $str=md5($str);
+		if (!mysql_num_rows($result)){
+			return "account_not_exist";
+		}
 		if ($password==$result['password']){
 			mysql_query("UPDATE `user` SET `session`='$str' WHERE '$username'=`username`");
 			$_SESSION['logindata']=$str;
 			$_SESSION['username']=$username;
-		}
+		}else{return "password_error";}
 	}
 	function check_status(){
 		$username=$_SESSION['username'];
@@ -23,6 +26,12 @@ class user{
 		}else{return "denied";}
 	}
 	function register($username,$password,$email){
-		
+		$query=mysql_query("SELECT * FROM `user` WHERE '$username'=`username`");
+		$result=mysql_fetch_array($query);
+		if (mysql_num_rows($result)){
+			return "exist";
+		}else{
+			$query=mysql_query("INSERT ")
+		}
 	}
 }
